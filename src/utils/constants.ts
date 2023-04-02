51d9617b7f0821303f89ca467af5ff8eaa3ff522
kharioki <tony.kharioki@gmail.com>
2023-04-02 15:43:41 +0300

    svg artworks and packages

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const INTRO_SCREEN_01 = {
+  title: 'Connect with your Favourite Service Providers',
+  description: 'Thank you for choosing us. We can save your lovely time.',
+};
+
+export const INTRO_SCREEN_02 = {
+  title: 'Book Appointments Easily',
+  description: 'Schedule your next session right from your phone.',
+};
+
+export const LOG_IN_SCREEN = {
+  title: "Let's Get started",
+  description: 'To register for an account, kindly enter your details',
+};
